bf486fc3db8a8e9a1a17b398b265fe984efdc28c
anishasnet <anishasnet@gmail.com>
2020-07-18 14:17:40 -0700

    Added fetch requests for service-worker

--- public/js/service-worker.js
+++ public/js/service-worker.js
@@ -45,3 +45,21 @@ self.addEventListener('activate', function(e) {
       })
     );
 });
+
+self.addEventListener('fetch', function(e) {
+    console.log('fetch request: ' + e.request.url)
+    e.respondWith(
+        caches.match(e.request)
+        .then(function(request) {
+            if(request) {
+                // if cache available -> return cache
+                console.log('responding with cache: ' + e.request.url)
+                return request
+            } else {
+                // no cache -> fetch request
+                console.log('file not cached, fetching: ' + e.request.url);
+                return fetch(e.request);
+            }
+        })
+    )
+})
