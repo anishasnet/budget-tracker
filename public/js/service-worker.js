f63f372ffff31e73e6614e2be990ee30141b92ae
anishasnet <anishasnet@gmail.com>
2020-07-18 10:49:08 -0700

    added service worker installation code

--- public/js/service-worker.js
+++ public/js/service-worker.js
@@ -13,3 +13,15 @@ const FILES_TO_CACHE = [
     "../icons/icon-512x512.png"
 ];
 
+const APP_PREFIX = 'BudgetTracker-';
+const VERSION = 'version_01';
+const CACHE_NAME = APP_PREFIX + VERSION;
+
+self.addEventListener('install', function (e) {
+    e.waitUntil(
+        caches.open(CACHE_NAME).then(function (cache) {
+          console.log('installing cache : ' + CACHE_NAME)
+          return cache.addAll(FILES_TO_CACHE)
+        })
+    )
+})
